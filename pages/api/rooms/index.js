b5ba3bc6f76fb9a56aca6c60954b269b8e0d5f1f
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2022-01-04 18:04:01 +0400

    Added room create endpoint

--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,9 +1,12 @@
 import nc from 'next-connect';
 
-import { allRooms } from '../../../controllers/roomControllers';
+import dbConnect from '../../../config/dbConnect';
+import { allRooms, newRoom } from '../../../controllers/roomControllers';
 
 const handler = nc();
 
+dbConnect();
 handler.get(allRooms);
+handler.post(newRoom);
 
 export default handler;
