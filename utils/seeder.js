88917ce6da2d5c6857229e0cf1b3617894426515
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2022-01-05 12:31:05 +0400

    Added rooms seeder

---
+++ utils/seeder.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Room = require("../models/room");
+const rooms = require("../data/rooms");
+
+mongoose.connect("mongodb://localhost:27017/bookit", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const seedRooms = async () => {
+  try {
+    await Room.deleteMany();
+    console.log("Rooms are deleted");
+
+    await Room.insertMany(rooms);
+    console.log("All rooms are inserted");
+    process.exit();
+  } catch (error) {
+    console.log(error.message);
+    process.exit();
+  }
+};
+
+seedRooms();
