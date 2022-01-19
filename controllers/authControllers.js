d74b335008f35ce6c802d6e76e8e31a00a793cb2
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2022-01-19 14:19:56 +0400

    Added cloudinary

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,10 +1,22 @@
 import User from "../models/user";
-
+import cloudinary from "cloudinary";
 import ErrorHandler from "../utils/errorHandler";
 import catchAsyncError from "../middlewares/catchAsyncError";
 import APIFeatures from "../utils/apiFeatures";
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
 const registerUser = catchAsyncError(async (req, res) => {
+  const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: 'bookit/avatars',
+    width: '150',
+    crop: 'scale'
+  });
+
   const { name, email, password } = req.body;
 
   const user = await User.create({
@@ -12,8 +24,8 @@ const registerUser = catchAsyncError(async (req, res) => {
     email,
     password,
     avatar: {
-      public_id: "PUBLIC_ID",
-      url: "URL",
+      public_id: result.public_id,
+      url: result.secret_url,
     },
   });
 
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   env: {
     DB_LOCAL_URI: "mongodb://localhost:27017/bookit",
+    CLOUDINARY_CLOUD_NAME: 'dfyeyzoof',
+    CLOUDINARY_API_KEY: '347557288216642',
+    CLOUDINARY_SECRET_KEY: 'c1DpWShs0wF8Je8fAUHUcw4eWvY'
   },
   images: {
     domains: ["res.cloudinary.com"],
